12c6658a9ff7bb23b2311194ece9ec60a3d111ba
Cuong Nguyen <cuongnguyen79140@gmail.com>
2025-10-23 21:14:59 +0700

    Remove all folders except ev-service-center-backend

--- README.md
+++
@@ -1,2 +0,0 @@
-# ev-service-center-management-system
-EV Service Center Maintenance Management System  - OOP Project
--- ev-service-center-backend/auth_service/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv"
-import app from "./src/app.js"
-
-dotenv.config()
-const PORT = process.env.PORT || 5001;
-
-app.litsen(PORT, ()=> {
-    console.log(`Auth Service running on port ${PORT}`)
-});
