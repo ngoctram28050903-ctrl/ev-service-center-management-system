b9768d9b1115d59faca95b12cba4ed9d2112f137
Cuong Nguyen <cuongnguyen79140@gmail.com>
2025-10-23 20:31:25 +0700

    Xóa folder ev-service-center-backend khỏi Git tracking (vẫn giữ local)

--- ev-service-center-backend/auth_service/src/models/role.js
+++
@@ -1,9 +0,0 @@
-import { DataTypes } from "sequelize";
-import seqelize from "../config/db.js";
-
-const Role = seqelize.define("Role", {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING, allowNull: false, unique: true },
-});
-
-export default Role;
--- ev-service-center-backend/booking_service/index.js
+++
@@ -1,4 +0,0 @@
-import app from './src/app.js';
-
-const PORT = process.env.PORT || 5002;
-app.listen(PORT, () => console.log(`🚀 Booking Service running on port ${PORT}`));
