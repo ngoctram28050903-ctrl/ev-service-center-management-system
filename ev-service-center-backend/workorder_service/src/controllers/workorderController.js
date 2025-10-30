309db5684cf26d7d720ffca2e03de40f44d95e4a
Chopper-97 <khangle0414@gmail.com>
2025-10-30 21:40:55 +0700

    sửa lần 1 Controller

--- ev-service-center-backend/workorder_service/src/controllers/workorderController.js
+++ ev-service-center-backend/workorder_service/src/controllers/workorderController.js
@@ -1,12 +1,24 @@
-import WorkOrder from '../models/workorder.js';
+import WorkOrder from '../models/workOrder.js';
 import ChecklistItem from '../models/checklistItem.js';
+import { Op } from 'sequelize';
+import sequelize from '../config/db.js';
+import { bookingClient, vehicleClient, userClient } from '../client/index.js';
 
-// Lấy tất cả work orders
-export const getAllWorkOrders = async (req, res) => {
+const updateWorkOrderTotalPrice = async (workOrderId) => {
   try {
-    const workOrders = await WorkOrder.findAll({ include: ChecklistItem });
-    res.json(workOrders);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    const workOrder = await WorkOrder.findByPk(workOrderId);
+    if (workOrder) {
+      const completedItems = await ChecklistItem.findAll({
+        where: { 
+          workOrderId: workOrderId,
+          completed: true 
+        },
+      });
+      
+      const totalPrice = completedItems.reduce((sum, item) => sum + item.price, 0);
+      await workOrder.update({ totalPrice });
+    }
+  } catch (error) {
+    console.error('Error updating work order total price:', error);
   }
 };
